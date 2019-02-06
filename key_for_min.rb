5088489864a9d26d8d1c87e425c4058d305b3306
Zachary <zbberman@yahoo.com>
2019-02-06 16:52:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   name_hash.each do | key, value |
     if min_val == nil 
       min_key = key 
-    elsif value < min_val || 
+    elsif value < min_val 
       min_key = key 
     end 
   end 
